fe3500896e3179cf1994f131831a5f65d4fbe475
Tarık Bugra Kocabıyık <tarikbugra9171@gmail.com>
2022-05-10 21:53:09 +0300

    Add project files.

---
+++ ExampleMailPublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using Inbx;
+using MassTransit;
+
+var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.Host(
+        "localhost",
+        5672,
+        "/",
+        "ExampleMailPublisher",
+        h =>
+        {
+            h.Username("admin");
+            h.Password("123456");
+        }
+    );
+});
+
+for (int i = 0; i < 1000; i++)
+{
+    await bus.Publish<IMessageMail>(new
+    {
+        Id = i,
+        Email = "tarik@useinbox.com",
+        Host = "123.123.12.23",
+        Port = 3253
+    });
+}
+
